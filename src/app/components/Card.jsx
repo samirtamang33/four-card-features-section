21aa75fbb15beaf97488adf0c73b864f6421eed1
Samir <samiirtamang@gmail.com>
2025-06-26 12:32:41 +0545

    component styled

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,11 +1,13 @@
 import styles from "./Card.module.css";
 
-export default function Card({ title, description, icon }) {
+export default function Card({ title, description, icon, borderColor }) {
   return (
-    <div className={styles.card}>
-      {icon && <div className={styles.icon}>{icon}</div>}
-      <h2 className={styles.cardTitle}>{title}</h2>
-      <p className={styles.cardDescription}>{description}</p>
+    <div className={styles.card} style={{ "--card-border-color": borderColor }}>
+      <div className={styles.cardContent}>
+        <h2 className={styles.cardTitle}>{title}</h2>
+        <p className={styles.cardDescription}>{description}</p>
+      </div>
+      <div className={styles.icon}>{icon}</div>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,24 +4,70 @@ import Card from "./components/Card";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div className={styles.titleWrapper}>
-          <div className={styles.title}>
-            <h1 className={styles.titleLite}>Reliable, efficient delivery</h1>
-            <h1 className={styles.titleBold}>Powered by Technology</h1>
-          </div>
-          <div className={styles.description}>
-            <p>
-              Our Artificial Intelligence powered tools use millions of project
-              data points to ensure that your project is successful.
-            </p>
-          </div>
+    <main className={styles.main}>
+      <div className={styles.titleWrapper}>
+        <div className={styles.title}>
+          <h1 className={styles.titleLite}>Reliable, efficient delivery</h1>
+          <h1 className={styles.titleBold}>Powered by Technology</h1>
         </div>
-        <div className={styles.cardsWrapper}>
-          <Card title="Test" description="test2" icon="icon" />
+        <div className={styles.description}>
+          <p>
+            Our Artificial Intelligence powered tools use millions of project
+            data points to ensure that your project is successful.
+          </p>
         </div>
-      </main>
-    </div>
+      </div>
+      <div className={styles.cardsWrapper}>
+        <Card
+          title="Supervisor"
+          description="Monitors activity to identify project roadblocks"
+          icon={
+            <Image
+              src="/assets/images/icon-supervisor.svg"
+              alt="Sample Icon"
+              width={64}
+              height={64}
+            />
+          }
+          borderColor="var(--cyan)"
+        />
+        <Card
+          title="Supervisor"
+          description="Monitors activity to identify project roadblocks"
+          icon={
+            <Image
+              src="/assets/images/icon-supervisor.svg"
+              alt="Sample Icon"
+              width={64}
+              height={64}
+            />
+          }
+        />
+        <Card
+          title="Supervisor"
+          description="Monitors activity to identify project roadblocks"
+          icon={
+            <Image
+              src="/assets/images/icon-supervisor.svg"
+              alt="Sample Icon"
+              width={64}
+              height={64}
+            />
+          }
+        />
+        <Card
+          title="Supervisor"
+          description="Monitors activity to identify project roadblocks"
+          icon={
+            <Image
+              src="/assets/images/icon-supervisor.svg"
+              alt="Sample Icon"
+              width={64}
+              height={64}
+            />
+          }
+        />
+      </div>
+    </main>
   );
 }
